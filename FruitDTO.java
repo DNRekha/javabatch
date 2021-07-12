fc5bc5ccc1eb4b2ea79599d82bb52a4cf00b6b1f
DNRekha <rekhadn.xworkz@gmail.com>
2021-07-13 00:15:54 +0530

    new branch

---
+++ FruitDTO.java
@@ -0,0 +1,20 @@
+public class FruitDTO{
+
+private String name;
+private int noOfFruit;
+
+public FruitDTO(){
+	} 
+ public String getName(){
+	 return name;
+ }
+ public void setName(String name){
+	 this.name=name;
+ }
+  public int getNoOfFruit(){
+	 return noOfFruit;
+ }
+ public void setNoOfFruit(int noOfFruit){
+	 this.noOfFruit=noOfFruit;
+ }
+}
